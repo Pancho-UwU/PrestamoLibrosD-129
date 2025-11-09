67cd093c9bb7bffdb766f65f6010c33ba8054820
Francisco Concha <Francisco.concha.urquieta@gmail.com>
2025-11-09 01:52:10 -0300

    refactor cors update

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -1,7 +1,9 @@
 import cors from 'cors'
 const corsOptions = {
-  origin: ['http://localhost:3000'], // agrega más orígenes si es necesario
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  origin: ['http://localhost:3000',
+    'https://prestamolibrosd-129.onrender.com/'
+  ], // agrega más orígenes si es necesario
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 };
